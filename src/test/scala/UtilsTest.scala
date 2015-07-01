b1c74689f7d4a217fe79784bd2a65d1307122a4a
Brynjar Smári Bjarnason <brynjar@spotify.com>
2015-07-01 12:15:59 +0200

    Tests and primes done

--- src/test/scala/UtilsTest.scala
+++ src/test/scala/UtilsTest.scala
@@ -2,5 +2,13 @@
  * Created by brynjar on 01/07/15.
  */
 class UtilsTest extends org.scalatest.FunSuite {
-
+  test("numsFrom generates natural numbers") {
+    assert(Utils.numsFrom(0).take(5).toList.last === 4L)
+  }
+  test("Fifth sieve prime is 11") {
+    assert(Utils.sieve(Utils.numsFrom(2)).take(5).toList.last === 11)
+  }
+  test("Sixth prime is 13") {
+    assert(Utils.primes.take(6).toList.last === 13)
+  }
 }
