5bbbef442176045609711fabb03636b9848e7ba9
Brynjar Smári Bjarnason <brynjar@spotify.com>
2015-07-01 12:15:24 +0200

    Tests and primes done

--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -1,8 +1,8 @@
 /**
  * Created by brynjar on 01/07/15.
  */
-class Utils {
+object Utils {
   def numsFrom(num: Long): Stream[Long] = num #:: numsFrom(num+1L)
   def sieve(s: Stream[Long]): Stream[Long] = s.head #:: sieve(s.tail.filter(_ % s.head != 0))
-
+  val primes = sieve(numsFrom(2L))
 }
